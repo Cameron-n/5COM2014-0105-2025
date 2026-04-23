b0815cb47f701a5e25a684bd4924018e93469b72
Cameron-n <cameron@keithbcook.com>
2026-04-23 23:59:43 +0100

    Add database jar and tables

---
+++ src/FairviewDatabaseSetup.java
@@ -0,0 +1,97 @@
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class FairviewDatabaseSetup {
+    public static void main(String[] args) {
+        //create database
+        String url = "jdbc:sqlite:fairview.db";
+
+        try (var conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                var meta = conn.getMetaData();
+                System.out.println("The driver name is " + meta.getDriverName());
+                System.out.println("A new database has been created.");
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+
+        var sql = "";
+
+        //create table: User
+        sql = "CREATE TABLE IF NOT EXISTS User ("
+            + "     userID INTEGER PRIMARY KEY,"
+            + "     name CHAR(255),"
+            + "     email VARCHAR(255),"
+            + "     password VARCHAR(255),"
+            + "     affiliation CHAR(255),"
+            + "     userType CHAR(255)"
+            + ");";
+
+        createTable(sql, url);
+
+        //create table: Applicant
+        sql = "CREATE TABLE IF NOT EXISTS Applicant ("
+            + "     userID INTEGER PRIMARY KEY,"
+            + "     FOREIGN KEY(userID) REFERENCES User(userID)"
+            + ");";
+
+        createTable(sql, url);
+
+        //create table: Reviewer
+        sql = "CREATE TABLE IF NOT EXISTS Reviewer ("
+            + "     userID INTEGER PRIMARY KEY,"
+            + "     reviewerCode VARCHAR(255),"
+            + "     FOREIGN KEY(userID) REFERENCES User(userID)"
+            + ");";
+
+        createTable(sql, url);
+
+        //create table: Talk
+        sql = "CREATE TABLE IF NOT EXISTS Talk ("
+            + "     talkID INTEGER PRIMARY KEY,"
+            + "     title CHAR(255),"
+            + "     description CHAR(255),"
+            + "     status CHAR(255),"
+            + "     applicantID INTEGER,"
+            + "     FOREIGN KEY(applicantID) REFERENCES Applicant(userID)"
+            + ");";
+
+        createTable(sql, url);
+
+        //create table: Review
+        sql = "CREATE TABLE IF NOT EXISTS Review ("
+            + "     reviewID INTEGER PRIMARY KEY,"
+            + "     score INTEGER,"
+            + "     feedback VARCHAR(255),"
+            + "     talkID INTEGER,"
+            + "     reviewerID INTEGER,"
+            + "     FOREIGN KEY(reviewerID) REFERENCES Reviewer(userID),"
+            + "     FOREIGN KEY(talkID) REFERENCES Talk(talkID)"
+            + ");";
+
+        createTable(sql, url);
+
+        //create table: Timeslot
+        sql = "CREATE TABLE IF NOT EXISTS Timeslot ("
+            + "     timeslotID INTEGER PRIMARY KEY,"
+            + "     startTime TIME,"
+            + "     endTime TIME,"
+            + "     talkID INTEGER,"
+            + "     FOREIGN KEY(talkID) REFERENCES Talk(talkID)"
+            + ");";
+
+        createTable(sql, url);
+
+    }
+
+    public static void createTable(String sql, String url) {
+        try (var conn = DriverManager.getConnection(url);
+             var stmt = conn.createStatement()) {
+            //create a new table
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
